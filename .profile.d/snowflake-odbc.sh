6413de7a98d8e46e62454c5847c47860b8404d26
Michael Carey <mikecx@gmail.com>
2022-06-15 16:19:42 -0700

    Removing the instance config.

--- .profile.d/snowflake-odbc.sh
+++ .profile.d/snowflake-odbc.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 
 export ODBCSYSINI=${HOME}/.apt/usr/lib/snowflake/odbc/conf/
-
-echo "[snowflake]
-Description=SnowflakeDB
-Driver=SnowflakeDSIIDriver
-Locale=en-US
-PORT=443
-SSL=on
-CLIENT_SESSION_KEEP_ALIVE=true
-" > ${ODBCSYSINI}/odbc.ini
